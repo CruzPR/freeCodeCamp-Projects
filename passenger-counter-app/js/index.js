8a5dea9b4447b09eb47bf2efe951ef1e327f9f14
Carlos <cruzprww2@gmail.com>
2022-05-01 14:51:27 -0500

    Continued working on the passenger-counter-app

--- passenger-counter-app/js/index.js
+++ passenger-counter-app/js/index.js
@@ -59,4 +59,8 @@ let greeting = "Hi, my name is ";
 let myGreeting = greeting + userName;
 console.log(myGreeting);
 
+console.log(4 + 5); // 9
+console.log("2" + "4"); // 24
+console.log("5" + 2); // 51
+console.log(100 + "100") // 100100
 
